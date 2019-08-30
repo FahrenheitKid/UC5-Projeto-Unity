c5e50800c29011069657b5712452c13725d26fbe
Fahrenheit <jordan.silvaa42@gmail.com>
2019-08-30 09:33:09 -0300

    Portal

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -10,6 +10,7 @@ public class Coin : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        
         //aqui apenas rotacionamos as moedas no eixo Y na velocidade rotationSpeed
         //como está no update, ela ficará rotacionando para sempre
         transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    Transform destino;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.transform.position = destino.position;
+        }
+    }
+}
